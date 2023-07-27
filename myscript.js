4c3c8e1ca08945c0b86850e821c522bf5621d923
Ellie Rabenold <rabenold@mit.edu>
2023-07-26 20:04:12 -0700

    hmm

--- myscript.js
+++ myscript.js
@@ -1,5 +1,17 @@
 function get_date(){
-    var node = document.getElementById("HELLO");
+    var node = document.getElementById("intro_date");
     var date = Date();
-    node.innerHTML = "$ Last login: " + date.slice(0,16);
+    node.innerHTML = " Last login: " + date.slice(0,16);
+}
+
+function hide_once(){
+    elm1 = document.getElementById("first_term");
+    elm2 = document.getElementById("second_term");
+    if(elm1.style.display !== "none"){
+        elm1.style.display = "none";
+    }  
+    if(elm2.style.display === "none"){
+        elm2.style.display = "block";
+    }   
+   // removeEventListener('click',hide_once) 
 }
